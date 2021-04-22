93a10b5743d0eb9f038915004f3bf3252c9d7476
LZQ1193545725 <34158698+LZQ1193545725@users.noreply.github.com>
2021-04-22 16:47:55 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace MyTest
         {
             int[] nums = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
             new AgorithmExample().BubbleSort(nums, SortType.ASC);
+            int[] nums1 = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            new AgorithmExample().InsertionSort(nums);
             Console.ReadLine();
         }
     }
